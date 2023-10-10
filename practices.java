bda31efde84b0fa1b4821da536876b7f56675942
RonCondas <124211322+RonCondas@users.noreply.github.com>
2023-10-10 11:29:33 -0500

    Add files via upload

---
+++ practices.java
@@ -0,0 +1,25 @@
+//
+//public class practices {
+//
+//	public static void main(String[] args) 
+//	{
+//		
+//		//1.Variables - Nouns
+//		int suit = 0; //Red suit
+//		int mask = 0; //mask color = red
+//		int weapon = 1; //pulse rocket out of his hand
+//		
+//		jump(); //call to the function name jump
+//		jump();
+//		fly();
+//		
+//	}
+//	public static void jump()
+//	{
+//		System.out.println("Ironman is jumping");
+//	}
+//		
+//		
+//	}
+//
+//
